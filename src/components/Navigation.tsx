cebbee0e2cea23536e2349b9a52273502cb4cabd
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-07-23 20:13:20 +0700

    Update

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -172,13 +172,25 @@ export const Navigation = (): JSX.Element => {
               href="/"
               className="dark:text-white text-[1.5rem] duration-300 transition-colors ease-out"
             >
-              <Image
-                src="/logo.svg"
-                width="30"
-                height="30"
-                className="w-[30px] dark:fill-white h-[30px]"
-                alt="logo"
-              />
+              <svg
+                xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+                xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:svg="http://www.w3.org/2000/svg"
+                version={1.0}
+                width={30}
+                height={30}
+                id="svg2"
+                className="dark:fill-white"
+                sodipodi:docname="Odal_rune.svg"
+                inkscape:version="1.2.2 (732a01da63, 2022-12-09)"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+              >
+                <path
+                  d="M 64.98808,279 0,213.4826 32.000356,180.52309 65.00665,213.48391 91.49048,187.00009 14.001338,109.49184 124.50682,0 234,109.4986 l -76.48123,77.49237 26.95661,27.0122 33.52393,-33.48042 L 250,213.49405 v 1.01749 L 184.48796,279 124.51251,219.99979 Z m 104.0121,-169.48351 -44.48369,-44.516669 -44.51667,44.483689 44.48369,44.51667 z"
+                  id="path2384"
+                />
+              </svg>
             </Link>
             <NavigationMenu>
               <NavigationMenuList>
