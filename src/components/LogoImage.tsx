aaf6d88426361e6b40b2dbfecf07087ef60a4b08
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-07-24 16:03:40 +0700

    Update

--- src/components/LogoImage.tsx
+++ src/components/LogoImage.tsx
@@ -1,6 +1,7 @@
+'use client'
 import Image from "next/image";
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const LogoImage = () => {
   const [src, setSrc] = useState('light.svg');
