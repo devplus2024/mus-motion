79f881bc0fd9ba0e0b101e15cbbb2329af145440
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-08-11 19:24:57 +0700

    Update

---
+++ main.js
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from "electron";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1040,
+    height: 670,
+    frame: false,
+    titleBarStyle: "hidden",
+    titleBarOverlay: {
+	  color:"#000",
+      symbolColor: "#fff",
+      height: 37,
+    }, // Loại bỏ khung mặc định của hệ điều hành
+  });
+
+  win.loadURL("http://localhost:3000/docs");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
