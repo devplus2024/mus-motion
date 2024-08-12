004b9c98018153ad7a489e9b35845c9836f7ded2
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-08-12 21:00:38 +0700

    Update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,11 +67,10 @@ const config = {
       },
       keyframes: {
 		meteor: {
-			  "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
-			  "70%": { opacity: 1 },
-			  "100%": {
-				transform: "rotate(215deg) translateX(-500px)",
-				opacity: 0,
+		"0%": { transform: "rotate(215deg) translateX(0px)", opacity: 1 },
+		"70%": { opacity: 1 },
+		"100%": {
+			transform: "rotate(215deg) translateX(-500px)",
 			},
 		},
         "border-beam": {
