28ee52224a752b80c02257f6c34fcc4a60da05bc
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-08-13 09:09:20 +0700

    Update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,24 +7,25 @@ export default function NotFound() {
       <div className=" w-[900px] gap-[2rem] h-[400px] flex flex-col items-center justify-center rounded-lg">
         <p className="text-3xl"> Oops, page not found!</p>
         <p>
-          The page you&apo;sre looking for doesn&apo;t exist or has been moved.
+          The page you`&apos;`aposre looking for doesn`&apos;`t exist or has
+          been moved.
         </p>
         <div className="flex gap-[1rem]">
           <Link
             href="/"
-            className="dark:bg-white px-3 py-1 bg-black text-white  dark:text-white"
+            className="dark:bg-white px-3 py-1 rounded-md bg-black text-white  dark:text-black"
           >
             Home
           </Link>
           <Link
             href="/docs"
-            className="dark:bg-white px-3 py-1 bg-black text-white  dark:text-white"
+            className="dark:bg-white px-3 py-1 rounded-md bg-black text-white  dark:text-black"
           >
             Docs
           </Link>
           <Link
             href="/pricing"
-            className="dark:bg-white px-3 py-1 bg-black text-white  dark:text-white"
+            className="dark:bg-white px-3 py-1 rounded-md bg-black text-white  dark:text-black"
           >
             Pricing
           </Link>
