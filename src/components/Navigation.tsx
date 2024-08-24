e391d1a031c459c930c27e35f858c082b1f84013
Phạm Quang Trường An <139692078+devplus2024@users.noreply.github.com>
2024-08-24 15:33:06 +0700

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -195,7 +195,7 @@ export const Navigation = (): JSX.Element => {
             <CommandMenu />
             <div className="flex gap-[1rem] items-center">
               <Link href="/downloads">
-                <Button variant={"outline"} className="dark:hover:bg-[#1a1a1a] hover:bg-muted">Download</Button>
+                <Button variant={"outline"} className="dark:hover:bg-[#1a1a1a] hover:bg-accent">Download</Button>
               </Link>
             </div>
           </div>
