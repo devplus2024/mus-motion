9b3589942fc573591d9da942028e1f841819402d
Phạm Quang Trường An <139692078+devplus2024@users.noreply.github.com>
2024-09-03 07:52:35 +0000

    Update

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
