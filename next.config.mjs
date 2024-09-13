f77886436f0f3c90c98afead414078bdfce60dad
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-09-13 15:50:47 +0700

    v2.6.5

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,10 @@
+import { createMDX } from "fumadocs-mdx/next";
+
+const withMDX = createMDX();
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  // othor next config here...
-  webpack: (config) => {
-    config.plugins.push(new VeliteWebpackPlugin());
-    return config;
-  },
+const config = {
+  reactStrictMode: true,
 };
 
-class VeliteWebpackPlugin {
-  static started = false;
-  apply(/** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs
-    // twice for the server (nodejs / edge runtime) and once for the client
-    compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
-      if (VeliteWebpackPlugin.started) return;
-      VeliteWebpackPlugin.started = true;
-      const dev = compiler.options.mode === "development";
-      const { build } = await import("velite");
-      await build({ watch: dev, clean: !dev });
-    });
-  }
-}
+export default withMDX(config);
