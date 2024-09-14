43614273a88c8e83867699f527f4e39fd1b347e8
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-09-14 15:17:04 +0700

    c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import "fumadocs-ui/style.css";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import Image from "next/image";
