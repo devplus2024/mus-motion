c605f7844cb79d29789e08e4318faf44934c36d0
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-09-14 14:36:13 +0700

    v2.6.5

---
+++ .source/index.js
@@ -0,0 +1,15 @@
+import { toRuntime } from "fumadocs-mdx";
+import * as file_0 from "../content/docs/index.mdx?collection=docs&hash=1f6bdb4ca1902e1116f178436a2432db3c32e973f175a17c551eba27edcb8dac";
+import * as file_1 from "../content/docs/test.mdx?collection=docs&hash=1f6bdb4ca1902e1116f178436a2432db3c32e973f175a17c551eba27edcb8dac";
+export const docs = [
+  toRuntime("doc", file_0, {
+    path: "index.mdx",
+    absolutePath:
+      "E:/Programming/Project/docs-musmotion/content/docs/index.mdx",
+  }),
+  toRuntime("doc", file_1, {
+    path: "test.mdx",
+    absolutePath: "E:/Programming/Project/docs-musmotion/content/docs/test.mdx",
+  }),
+];
+export const meta = [];
