6c5cf8f6c24a41f816a8c5eb4c23dbc6f00d7bac
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-09-14 14:56:34 +0700

    v2.6.5

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,17 +1,12 @@
-import { DocsLayout } from "fumadocs-ui/layout";
-import type { ReactNode } from "react";
-import { baseOptions } from "../layout.config";
-import { source } from "@/app/source";
 import type { Metadata } from "next";
-import "fumadocs-ui/style.css";
 export const metadata: Metadata = {
-  title: "Documention | MusMotion",
-  description: "MusicHub App",
+  title: "Docs | MusMotion",
+  description: "MusMotion - Documention",
 };
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
-      {children}
-    </DocsLayout>
-  );
+export default function CommunityLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <section>{children}</section>;
 }
