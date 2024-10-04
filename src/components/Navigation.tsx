81e43de60e537a9658b6c69222f9a40694c31b4e
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-10-04 19:27:13 +0700

    v10.7.8

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -272,7 +272,7 @@ export const Navigation = (): JSX.Element => {
               <ThemeToggle />
               <CommandMenu />
             </div>
-            <div className="flex items-center gap-[2rem]">
+            <div className="flex items-center gap-[1rem]">
               <Link
                 className="flex items-center justify-center gap-3"
                 href="/downloads"
