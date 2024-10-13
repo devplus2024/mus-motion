087c135456850f8180cb4d1ce93358f3643a4d59
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-10-13 14:06:23 +0700

    cv

--- src/app/radio/page.tsx
+++ src/app/radio/page.tsx
@@ -163,11 +163,34 @@ export default function Component() {
                 viewBox="0 0 16 16"
                 width={16}
                 style={{ color: "currentcolor" }}
+                className={`${isPlaying ? "hidden" : "block"}`}
+                onClick={() => {
+                  setIsPlaying(true);
+                }}
               >
                 <path
                   fillRule="evenodd"
                   clipRule="evenodd"
                   d="M14.5528 7.77638C14.737 7.86851 14.737 8.13147 14.5528 8.2236L1.3618 14.8191C1.19558 14.9022 1 14.7813 1 14.5955L1 1.4045C1 1.21865 1.19558 1.09778 1.3618 1.18089L14.5528 7.77638Z"
+                  fill="currentColor"
+                />
+              </svg>
+              <svg
+                data-testid="geist-icon"
+                height={16}
+                strokeLinejoin="round"
+                viewBox="0 0 16 16"
+                width={16}
+                style={{ color: "currentcolor" }}
+                className={`${!isPlaying ? "hidden" : "block"}`}
+                onClick={() => {
+                  setIsPlaying(false);
+                }}
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M5.5 2.5V1.75H4V2.5V13.5V14.25H5.5V13.5V2.5ZM12 2.5V1.75H10.5V2.5V13.5V14.25H12V13.5V2.5Z"
                   fill="currentColor"
                 />
               </svg>
