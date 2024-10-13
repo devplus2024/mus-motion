ae98ed9ec7f79a2b536b1b88938104ef8f96c484
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-10-13 14:12:28 +0700

    cv

--- src/app/radio/page.tsx
+++ src/app/radio/page.tsx
@@ -140,76 +140,84 @@ export default function Component() {
           </div>
           <div className="flex w-[20rem] flex-col items-center justify-center gap-[1rem]">
             <div className="flex gap-[2rem]">
-              <svg
-                data-testid="geist-icon"
-                height={16}
-                strokeLinejoin="round"
-                viewBox="0 0 16 16"
-                width={16}
-                style={{ color: "currentcolor" }}
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M4.08144 8.21092C3.92706 8.11268 3.92706 7.88733 4.08144 7.78909L14.3658 1.24451C14.5322 1.1386 14.75 1.25815 14.75 1.45542L14.75 14.5446C14.75 14.7419 14.5322 14.8614 14.3658 14.7555L4.08144 8.21092ZM0.75 2V1.25H2.25V2V14V14.75H0.75V14V2Z"
-                  fill="currentColor"
-                />
-              </svg>
+              <Button variant={"outline"} size={"icon"}>
+                <svg
+                  data-testid="geist-icon"
+                  height={16}
+                  strokeLinejoin="round"
+                  viewBox="0 0 16 16"
+                  width={16}
+                  style={{ color: "currentcolor" }}
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M4.08144 8.21092C3.92706 8.11268 3.92706 7.88733 4.08144 7.78909L14.3658 1.24451C14.5322 1.1386 14.75 1.25815 14.75 1.45542L14.75 14.5446C14.75 14.7419 14.5322 14.8614 14.3658 14.7555L4.08144 8.21092ZM0.75 2V1.25H2.25V2V14V14.75H0.75V14V2Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </Button>
 
-              <svg
-                data-testid="geist-icon"
-                height={16}
-                strokeLinejoin="round"
-                viewBox="0 0 16 16"
-                width={16}
-                style={{ color: "currentcolor" }}
-                className={`${isPlaying ? "hidden" : "block"}`}
-                onClick={() => {
-                  setIsPlaying(true);
-                }}
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M14.5528 7.77638C14.737 7.86851 14.737 8.13147 14.5528 8.2236L1.3618 14.8191C1.19558 14.9022 1 14.7813 1 14.5955L1 1.4045C1 1.21865 1.19558 1.09778 1.3618 1.18089L14.5528 7.77638Z"
-                  fill="currentColor"
-                />
-              </svg>
-              <svg
-                data-testid="geist-icon"
-                height={16}
-                strokeLinejoin="round"
-                viewBox="0 0 16 16"
-                width={16}
-                style={{ color: "currentcolor" }}
-                className={`${!isPlaying ? "hidden" : "block"}`}
-                onClick={() => {
-                  setIsPlaying(false);
-                }}
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M5.5 2.5V1.75H4V2.5V13.5V14.25H5.5V13.5V2.5ZM12 2.5V1.75H10.5V2.5V13.5V14.25H12V13.5V2.5Z"
-                  fill="currentColor"
-                />
-              </svg>
+              <Button variant="outline" size="icon">
+                <svg
+                  data-testid="geist-icon"
+                  height={16}
+                  strokeLinejoin="round"
+                  viewBox="0 0 16 16"
+                  width={16}
+                  style={{ color: "currentcolor" }}
+                  className={`${isPlaying ? "hidden" : "block"}`}
+                  onClick={() => {
+                    setIsPlaying(true);
+                  }}
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M14.5528 7.77638C14.737 7.86851 14.737 8.13147 14.5528 8.2236L1.3618 14.8191C1.19558 14.9022 1 14.7813 1 14.5955L1 1.4045C1 1.21865 1.19558 1.09778 1.3618 1.18089L14.5528 7.77638Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </Button>
+              <Button variant="outline" size="icon">
+                <svg
+                  data-testid="geist-icon"
+                  height={16}
+                  strokeLinejoin="round"
+                  viewBox="0 0 16 16"
+                  width={16}
+                  style={{ color: "currentcolor" }}
+                  className={`${!isPlaying ? "hidden" : "block"}`}
+                  onClick={() => {
+                    setIsPlaying(false);
+                  }}
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M5.5 2.5V1.75H4V2.5V13.5V14.25H5.5V13.5V2.5ZM12 2.5V1.75H10.5V2.5V13.5V14.25H12V13.5V2.5Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </Button>
 
-              <svg
-                data-testid="geist-icon"
-                height={16}
-                strokeLinejoin="round"
-                viewBox="0 0 16 16"
-                width={16}
-                style={{ color: "currentcolor" }}
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M11.6686 8.21092C11.8229 8.11268 11.8229 7.88733 11.6686 7.78909L1.38422 1.24451C1.21779 1.1386 1 1.25815 1 1.45542V14.5446C1 14.7419 1.21779 14.8614 1.38422 14.7555L11.6686 8.21092ZM15 2V1.25H13.5V2V14V14.75H15V14V2Z"
-                  fill="currentColor"
-                />
-              </svg>
+              <Button variant="outline" size={"icon"}>
+                <svg
+                  data-testid="geist-icon"
+                  height={16}
+                  strokeLinejoin="round"
+                  viewBox="0 0 16 16"
+                  width={16}
+                  style={{ color: "currentcolor" }}
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M11.6686 8.21092C11.8229 8.11268 11.8229 7.88733 11.6686 7.78909L1.38422 1.24451C1.21779 1.1386 1 1.25815 1 1.45542V14.5446C1 14.7419 1.21779 14.8614 1.38422 14.7555L11.6686 8.21092ZM15 2V1.25H13.5V2V14V14.75H15V14V2Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </Button>
             </div>
             <div className="">
               <Slider
