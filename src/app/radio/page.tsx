4f400bf4082d792cd6379531e90aab6c0b437056
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-10-14 19:08:43 +0700

    cv

--- src/app/radio/page.tsx
+++ src/app/radio/page.tsx
@@ -64,10 +64,8 @@ export default function Component() {
     if (audioRef.current) {
       if (audioRef.current.paused) {
         audioRef.current.play();
-        setIsPlaying(true);
       } else {
         audioRef.current.pause();
-        setIsPlaying(false);
       }
     }
   };
