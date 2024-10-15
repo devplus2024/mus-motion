f207636f968e70992b07f4a063dccc037d0ac790
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-10-15 17:30:28 +0700

    xc

--- src/app/radio/page.tsx
+++ src/app/radio/page.tsx
@@ -28,7 +28,7 @@ export default function Component() {
   const [currentStation, setCurrentStation] = useState("Synthwave Nights");
   const [currentGenre, setCurrentGenre] = useState("Electronic");
   const [currentTimeMusic, setCurrentTimeMusic] = useState<string>("3:40");
-  const [value, setValue] = useState<number[]>([50]);
+  const [value, setValue] = useState<number[]>([0]);
   const [currentTime, setCurrentTime] = useState<number>(0);
   const stations = [
     { name: "Synthwave Nights", genre: "Electronic", time: "3:40" },
