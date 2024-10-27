cb3bb373b00cf3f15e8f49b43209dc539d8c927c
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-10-27 19:26:59 +0700

    cv

--- src/app/home/components/chart-line.tsx
+++ src/app/home/components/chart-line.tsx
@@ -17,7 +17,7 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 
-export const description = "An interactive bar chart";
+export const description = "An interactive line chart";
 
 const chartData = [
   { date: "2024-04-01", desktop: 222, mobile: 150 },
@@ -119,15 +119,15 @@ const chartConfig = {
   },
   desktop: {
     label: "Desktop",
-    color: "hsl(var(--chart-bar-web))",
+    color: "hsl(var(--chart-1))",
   },
   mobile: {
     label: "Mobile",
-    color: "hsl(var(--chart-bar-web))",
+    color: "hsl(var(--chart-2))",
   },
 } satisfies ChartConfig;
 
-export function ChartBar() {
+export function ChartLine() {
   const [activeChart, setActiveChart] =
     React.useState<keyof typeof chartConfig>("desktop");
 
@@ -140,28 +140,28 @@ export function ChartBar() {
   );
 
   return (
-    <Card className="w-full border-none bg-black">
-      <CardHeader className="flex flex-col items-stretch space-y-0 rounded-lg border p-2 sm:flex-row">
-        <div className="flex flex-1 flex-col justify-center gap-1 px-6">
-          <CardTitle>Bar Chart - Interactive</CardTitle>
+    <Card>
+      <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
+        <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
+          <CardTitle>Line Chart - Interactive</CardTitle>
           <CardDescription>
             Showing total visitors for the last 3 months
           </CardDescription>
         </div>
         <div className="flex">
-          {["desktop", "mobile"].map((key: string) => {
+          {["desktop", "mobile"].map((key) => {
             const chart = key as keyof typeof chartConfig;
             return (
               <button
                 key={chart}
                 data-active={activeChart === chart}
-                className="relative z-30 flex flex-1 flex-col justify-center gap-1 rounded-lg px-6 py-px text-left data-[active=true]:bg-[#0c0c0c] sm:px-8"
+                className="flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
                 onClick={() => setActiveChart(chart)}
               >
-                <span className="text-xs data-[active=true]:text-black">
+                <span className="text-xs text-muted-foreground">
                   {chartConfig[chart].label}
                 </span>
-                <span className="text-lg font-bold leading-none data-[active=true]:text-black sm:text-3xl">
+                <span className="text-lg font-bold leading-none sm:text-3xl">
                   {total[key as keyof typeof total].toLocaleString()}
                 </span>
               </button>
@@ -169,7 +169,7 @@ export function ChartBar() {
           })}
         </div>
       </CardHeader>
-      <CardContent className="px-6 pt-[2rem]">
+      <CardContent className="px-2 sm:p-6">
         <ChartContainer
           config={chartConfig}
           className="aspect-auto h-[250px] w-full"
