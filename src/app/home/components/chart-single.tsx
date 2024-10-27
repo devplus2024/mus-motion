c9fc6363e60323b673c6499357157a8a2a1365e6
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-10-27 20:08:39 +0700

    dfdf

--- src/app/home/components/chart-single.tsx
+++ src/app/home/components/chart-single.tsx
@@ -27,6 +27,12 @@ const chartData = [
   { month: "April", desktop: 73, mobile: 190 },
   { month: "May", desktop: 209, mobile: 130 },
   { month: "June", desktop: 214, mobile: 140 },
+  { month: "July", desktop: 520, mobile: 320 },
+  { month: "August", desktop: 328, mobile: 120 },
+  { month: "September", desktop: 650, mobile: 458 },
+  { month: "October", desktop: 234, mobile: 130 },
+  { month: "November", desktop: 514, mobile: 260 },
+  { month: "December", desktop: 610, mobile: 240 },
 ];
 
 const chartConfig = {
