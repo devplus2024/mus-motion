cf7e26f6eaeded063506b92773d60d3ca8f07892
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-10-27 10:03:20 +0700

    xc

--- .source/index.d.ts
+++ .source/index.d.ts
@@ -1,3 +1,3 @@
-import type { GetOutput } from "fumadocs-mdx/config";
-export declare const docs: GetOutput<typeof import("../source.config.js").docs>;
-export declare const meta: GetOutput<typeof import("../source.config.js").meta>;
+import type { GetOutput } from "fumadocs-mdx/config"
+export declare const docs: GetOutput<typeof import("../source.config.ts").docs>
+export declare const meta: GetOutput<typeof import("../source.config.ts").meta>
