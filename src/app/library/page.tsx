f8e44a044e75980f60cb56c3261c0acf8609e6b8
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-11-03 14:28:49 +0700

    cv

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -40,7 +40,7 @@ const fetchSpotifyData = async (endpoint: string, accessToken: string) => {
 
 const refreshAccessToken = async () => {
   try {
-    const res = await fetch("/api/refresh", {
+    const res = await fetch("/api/refresh-token", {
       method: "POST",
     });
 
