e4a3a0292dfa600ee747e5ac8eb9ab1689f1df4a
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-11-06 17:59:07 +0700

    cv

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,9 +8,10 @@ export default function Footer() {
   const isAi = path === "/ai";
   const isChatV2 = path === "/chat-v2";
   const isRadio = path === "/radio";
+  const isLogin = path === "/login";
   return (
     <div
-      className={`${isWebApp || isAi || isChatV2 || isRadio ? "webfilm-class" : ""} h-[60px] border-t`}
+      className={`${isWebApp || isAi || isChatV2 || isRadio || isLogin ? "webfilm-class" : ""} h-[60px] border-t`}
     ></div>
   );
 }
