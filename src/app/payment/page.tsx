32c44303f93df1496e3d8b97d2898c97582fd396
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-11-17 08:23:59 +0700

    Stroma Update 11/16/2024 8:42:20

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -18,6 +18,12 @@ import {
 } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
 import {
+  CaretSortIcon,
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from "@radix-ui/react-icons";
+import {
   Card,
   CardContent,
   CardDescription,
@@ -298,8 +304,8 @@ export default function PayMentPage() {
         >
           {value
             ? countries.find((country) => country.value === value)?.label
-            : "Select Country..."}
-          <ChevronsUpDown className="opacity-50 w-[10px]  h-[10px] " />
+            : "Select Country"}
+          < CaretSortIcon className="opacity-50 w-4  h-4 " />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
@@ -318,9 +324,9 @@ export default function PayMentPage() {
                   }}
                 >
                   {country.label}
-                  <Check
+                  <CheckIcon
                     className={cn(
-                      "ml-auto",
+                      "ml-auto h-4 w-4",
                       value === country.value ? "opacity-100" : "opacity-0"
                     )}
                   />
@@ -363,23 +369,7 @@ export default function PayMentPage() {
                     placeholder="1234 5678 9012 3456"
                     required
                   />
-                  <svg
-  data-testid="geist-icon"
-  className="lucide lucide-lock absolute right-3 top-1/2 -translate-y-1/2 transform text-muted-foreground"
-  height={16}
-  strokeLinejoin="round"
-  viewBox="0 0 16 16"
-  width={16}
-  style={{ color: "currentcolor" }}
->
-  <path
-    fillRule="evenodd"
-    clipRule="evenodd"
-    d="M10 4.5V6H6V4.5C6 3.39543 6.89543 2.5 8 2.5C9.10457 2.5 10 3.39543 10 4.5ZM4.5 6V4.5C4.5 2.567 6.067 1 8 1C9.933 1 11.5 2.567 11.5 4.5V6H12.5H14V7.5V12.5C14 13.8807 12.8807 15 11.5 15H4.5C3.11929 15 2 13.8807 2 12.5V7.5V6H3.5H4.5ZM11.5 7.5H10H6H4.5H3.5V12.5C3.5 13.0523 3.94772 13.5 4.5 13.5H11.5C12.0523 13.5 12.5 13.0523 12.5 12.5V7.5H11.5Z"
-    fill="currentColor"
-  />
-</svg>
-
+                  
                 </div>
                 {errors.card && (
                   <p className="text-sm text-red-500">{errors.card}</p>
