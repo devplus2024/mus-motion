5d683b5c168d37e21b2959474ac0638e7135b63a
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-11-20 15:00:01 +0700

    vb

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,5 +1,10 @@
+import { Banner } from "@/components/banner";
 import React from "react";
 
 export default function SupportPage() {
-  return <div>Support</div>;
+  return (
+    <div>
+      <Banner></Banner>
+    </div>
+  );
 }
