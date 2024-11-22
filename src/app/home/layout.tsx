135c07179fe88631118052ece0fcb95176b99ac2
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-11-22 17:27:43 +0700

    Stroma Update 11/16/2024 8:42:20

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
       style={{ colorScheme: "dark" }}
     >
       <body className="relative overflow-x-hidden dark:[color-scheme:dark]">
+	   <Toaster />
         {children}
       </body>
     </html>
