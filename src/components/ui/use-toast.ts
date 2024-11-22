6139d0b02687f51bf85fdc82043a7c8561fea70f
Phạm Quang Trường An <139692078+devplus2024@users.noreply.github.com>
2024-11-22 20:13:13 +0700

    Update use-toast.ts

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -8,8 +8,8 @@ import type {
   ToastProps,
 } from "@/components/ui/toast"
 
-const TOAST_LIMIT = 1
-const TOAST_REMOVE_DELAY = 1000000
+const TOAST_LIMIT = 3
+const TOAST_REMOVE_DELAY = 1
 
 type ToasterToast = ToastProps & {
   id: string
