f67bee8c638d298b0f9b0975c5f7b0e24edf8e3c
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-11-26 17:35:26 +0700

    cv

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -14,7 +14,7 @@ export function Toaster() {
   const { toasts } = useToast();
 
   return (
-    <ToastProvider swipeDirection="up">
+    <ToastProvider swipeDirection="down">
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
