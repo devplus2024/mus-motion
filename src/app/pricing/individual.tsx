dfeeb31512e81febeda8fd890691fa4d19dd38cc
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-12-05 21:29:13 +0700

    first commit

--- src/app/pricing/individual.tsx
+++ src/app/pricing/individual.tsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
-import { useState } from "react";
 import { ToastAction } from "@/components/ui/toast";
 import {
   CreditCard,
