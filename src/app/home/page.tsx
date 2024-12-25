79e136cc8e0ec06332ae0c05ec1a63af27fbdda3
devplus2024 <anpham0812009@gmail.com>
2024-12-25 21:18:17 +0700

    vb

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -435,7 +435,7 @@ export default function Home() {
         <div className="flex gap-[2rem]">
           <DownloadButton />
           <Link href="/webapp">
-            <Button className="hover:bg-accent dark:hover:bg-[#1a1a1a] min-[375px]:w-full">
+            <Button className="">
               <svg
                 className="mr-2 h-4 w-4 dark:invert-[1]"
                 data-testid="geist-icon"
