ae911d7a847ecef996a9931c2476bb002a520d40
devplus2024 <anpham0812009@gmail.com>
2024-12-25 19:27:03 +0700

    vb

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -472,8 +472,8 @@ export default function Home() {
             ></Image>
           ))}
         </InfiniteSlider>
-        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
-        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+        <div className="overlay"></div>
+        <div className="overlay right"></div>
       </div>
       <div className="mt-[2rem] flex w-full items-center justify-center gap-[4rem] px-[2rem]">
         <ChartFirstYear />
