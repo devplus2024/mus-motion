e5c8a11362c7d62149ad13aedf6b702394b909e8
devplus2024 <anpham0812009@gmail.com>
2024-12-29 19:12:37 +0700

    vb

--- src/components/ui/tailwindcss-buttons.tsx
+++ src/components/ui/tailwindcss-buttons.tsx
@@ -1,6 +1,6 @@
 export default function TailwindcssButton() {
   return (
-    <button className="relative inline-block cursor-pointer rounded-full border p-px text-xs font-semibold leading-6 text-white no-underline shadow-2xl">
+    <button className="relative inline-block cursor-pointer rounded-md border p-px text-xs font-semibold leading-6 text-white no-underline shadow-2xl">
       <span className="absolute inset-0 overflow-hidden rounded-full">
         <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
       </span>
