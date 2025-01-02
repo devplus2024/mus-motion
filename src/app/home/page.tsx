76314c5996cb1f75b484a8b549091d589dd07614
devplus2024 <anpham08112009@gmail.com>
2025-01-02 21:38:38 +0700

    VB

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -217,7 +217,9 @@ export default function Home() {
           <span className="spark mask-gradient animate-flip before:animate-rotate absolute inset-0 h-[100%] w-[100%] overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
         </span>
         <span className="backdrop absolute inset-px rounded-full bg-neutral-950 transition-colors duration-200" />
-        <span className="z-10 text-xs font-medium text-neutral-400">Badge</span>
+        <span className="z-10 text-xs font-medium text-neutral-400">
+          Introducing Stroma v10.9.5
+        </span>
       </div>
       <motion.div
         initial={{ opacity: 0, y: 20 }} // Trạng thái ban đầu: mờ và di chuyển xuống
