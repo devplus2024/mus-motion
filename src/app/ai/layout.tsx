95c089ca4fcf5983cab26ebf27b111fe0ea8475e
Phạm Quang Trường An <139692078+developerplus2025@users.noreply.github.com>
2025-02-02 17:50:13 +0700

    Delete src/app/ai directory

--- src/app/ai/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "AI | MusMoitoin",
-  description: "Stroma - AI",
-};
-export default function AiLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
-}
