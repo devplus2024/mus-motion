33721127924fa712cad44e6c47b8801545fde38e
devplus2024 <anpham08112009@gmail.com>
2025-02-03 20:17:51 +0700

    vb

--- src/app/home/components/people-say.tsx
+++ src/app/home/components/people-say.tsx
@@ -143,7 +143,26 @@ export function PeopleSay() {
               <div className="absolute -bottom-[10px] -left-[10px] h-[10px] w-[10px] border-r border-t"></div>
               <div className="absolute -bottom-[10px] -right-[10px] h-[10px] w-[10px] border-l border-t"></div>
 <div className=" flex items-start flex-col gap-1 text-sm leading-tight sm:text-base">
-  <p className="text-xl ">{item.name}</p>
+  <div className="w-full flex justify-between">
+    
+    <p className="text-xl ">{item.name}</p>
+    <svg
+  data-testid="geist-icon"
+  height={16}
+  strokeLinejoin="round"
+  viewBox="0 0 16 16"
+  width={16}
+  style={{ color: "currentcolor" }}
+>
+  <path
+    fillRule="evenodd"
+    clipRule="evenodd"
+    d="M0.5 0.5H5.75L9.48421 5.71053L14 0.5H16L10.3895 6.97368L16.5 15.5H11.25L7.51579 10.2895L3 15.5H1L6.61053 9.02632L0.5 0.5ZM12.0204 14L3.42043 2H4.97957L13.5796 14H12.0204Z"
+    fill="currentColor"
+  />
+</svg>
+
+    </div>
   <p className="text-sm text-[#a1a1a1]">{item.username}</p>
   </div>
   <span className="text-muted-foreground text-md">{item.body}</span>
