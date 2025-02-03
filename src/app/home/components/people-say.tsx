b4bc99780e782d3b0ade0a96b20deb4a6cf1c9ad
devplus2024 <anpham08112009@gmail.com>
2025-02-03 20:15:38 +0700

    vb

--- src/app/home/components/people-say.tsx
+++ src/app/home/components/people-say.tsx
@@ -137,16 +137,16 @@ export function PeopleSay() {
   return (
     <div   className="grid grid-cols-3 justify-items-center gap-[2rem] ">
       {items.map((item) => (
-        <div key={item.name} className="flex flex-cols gap relative w-[300px] flex flex-col gap-1  border bg-card p-4 text-card-foreground shadow-sm">
+        <div key={item.name} className="flex flex-cols gap relative w-[400px] flex flex-col gap-1  border bg-card p-4 text-card-foreground shadow-sm">
            <div className="absolute -left-[10px] -top-[10px] h-[10px] w-[10px] border-b border-r"></div>
               <div className="absolute -right-[10px] -top-[10px] h-[10px] w-[10px] border-b border-l"></div>
               <div className="absolute -bottom-[10px] -left-[10px] h-[10px] w-[10px] border-r border-t"></div>
               <div className="absolute -bottom-[10px] -right-[10px] h-[10px] w-[10px] border-l border-t"></div>
 <div className=" flex items-start flex-col gap-1 text-sm leading-tight sm:text-base">
-  <p className="text-sm ">{item.name}</p>
-  <p className="text-xs text-[#a1a1a1]">{item.username}</p>
+  <p className="text-xl ">{item.name}</p>
+  <p className="text-sm text-[#a1a1a1]">{item.username}</p>
   </div>
-  <span className="text-muted-foreground text-sm">{item.body}</span>
+  <span className="text-muted-foreground text-md">{item.body}</span>
         </div>
       ))}
     </div>
