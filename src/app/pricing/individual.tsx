4c0e01604007b339d3c15e7cffa3e57df6ef2157
devplus2024 <anpham08112009@gmail.com>
2025-02-05 20:48:13 +0700

    vb

--- src/app/pricing/individual.tsx
+++ src/app/pricing/individual.tsx
@@ -867,13 +867,33 @@ export default function Individual() {
               className={`${tier.title === "Free" ? "to-[#121212]" : tier.title === "Basic" ? "to-[#121212]" : tier.title === "Pro" ? "to-[#121212]" : "to-[#121212]"} space-y-2 rounded-t-lg bg-[#00000099] p-[1.5rem]`}
             >
               <CardTitle className="text-[1.04rem] font-medium text-[#a1a1a1]">
-                <p
+                <div className="flex justify-between items-center">
+                   <p
                   className={`${tier.title === "Pro" || tier.title === "Basic" ? "hidden" : ""}`}
                 >
                   {tier.title}
                 </p>
+                <svg
+                className={`${tier.title === "Pro" || tier.title === "Basic" ? "hidden" : ""}`}
+  data-testid="geist-icon"
+  height={16}
+  strokeLinejoin="round"
+  style={{ color: "currentcolor" }}
+  viewBox="0 0 16 16"
+  width={16}
+>
+  <path
+    fillRule="evenodd"
+    clipRule="evenodd"
+    d="M8 14.5C11.5899 14.5 14.5 11.5899 14.5 8C14.5 4.41015 11.5899 1.5 8 1.5C4.41015 1.5 1.5 4.41015 1.5 8C1.5 11.5899 4.41015 14.5 8 14.5ZM8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM6.25 7H7H7.74999C8.30227 7 8.74999 7.44772 8.74999 8V11.5V12.25H7.24999V11.5V8.5H7H6.25V7ZM8 6C8.55229 6 9 5.55228 9 5C9 4.44772 8.55229 4 8 4C7.44772 4 7 4.44772 7 5C7 5.55228 7.44772 6 8 6Z"
+    fill="currentColor"
+  />
+</svg>
+
+                </div>
+               
                 {tier.title === "Pro" && (
-                  <div className="flex justify-between">
+                  <div className="flex items-center justify-between">
                     <p>Pro</p>
                     <Badge className="h-fit px-3 py-1" variant={"outline"}>
                       Popular
